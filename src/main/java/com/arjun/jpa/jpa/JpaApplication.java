9df52d9cb95f48e48f866ed67e16cf54645106c8
arjunpariyar <punksarjun@gmail.com>
2023-12-24 19:31:42 +0900

    sql configured

--- src/main/java/com/arjun/jpa/jpa/JpaApplication.java
+++ src/main/java/com/arjun/jpa/jpa/JpaApplication.java
@@ -2,8 +2,12 @@ package com.arjun.jpa.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@PropertySource("classpath:config.properties")
 public class JpaApplication {
 
 	public static void main(String[] args) {

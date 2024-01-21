599e163dad104e26fdbfbfca1bc5cf3b18222480
arjunpariyar <punksarjun@gmail.com>
2024-01-21 18:07:17 +0900

    added config properties

--- src/main/java/com/arjun/jpa/jpa/JpaApplication.java
+++ src/main/java/com/arjun/jpa/jpa/JpaApplication.java
@@ -2,6 +2,8 @@ package com.arjun.jpa.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableConfigurationProperties
